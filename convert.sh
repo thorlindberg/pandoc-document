7328cd577e942e290bc91340698f543a8fb15ad6
Thor Lindberg <71700400+thorlindberg@users.noreply.github.com>
2022-11-20 00:14:29 +0100

    .tex output now removed post-transpile

--- convert.sh
+++ convert.sh
@@ -1,2 +1,3 @@
 pandoc preamble.yaml --defaults files.yaml -o output/output.tex -f markdown -t latex
 pandoc -i output/output.tex -o output/output.docx --reference-doc reference.docx -f latex -t docx
+rm output/output.tex
